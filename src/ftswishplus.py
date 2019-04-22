20ade224fc9a61e19fc20be8f1c5e2ce98116781
lessw2020 <lessw@etrillium.com>
2019-04-22 08:43:28 -0700

    simplified with F.relu

--- src/ftswishplus.py
+++ src/ftswishplus.py
@@ -1,25 +1,19 @@
 #original FTSwish = https://arxiv.org/abs/1812.06247
 
+#import torch.nn.functional as F  (uncomment if needed,but you likely already have it)
+
 class FTSwishPlus(nn.Module):
     def __init__(self, threshold=-.25, mean_shift=-.1):
         super().__init__()
         self.threshold = threshold
         self.mean_shift = mean_shift
-        #warning - does not handle multi-gpu case below
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
-
 
     def forward(self, x): 
-        
-        #FTSwish+ for positive values
-        pos_value = (x*torch.sigmoid(x)) + self.threshold
-        
-        #FTSwish+ for negative values
-        tval = torch.tensor([self.threshold],device=self.device)
-        
-        #apply to x tensor based on positive or negative value
-        x = torch.where(x>=0, pos_value, tval)
-        
+
+        x = F.relu(x) * torch.sigmoid(x) + self.threshold
+        #note on above - why not F.sigmoid?: 
+        #PyTorch docs - "nn.functional.sigmoid is deprecated. Use torch.sigmoid instead."
+
         #apply mean shift to drive mean to 0. -.1 was tested as optimal for kaiming init
         if self.mean_shift is not None:
             x.sub_(self.mean_shift)
